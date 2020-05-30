b3939d831a616894eecf09ac61e90e84dc7c7e69
uruwanara <uruwanara@users.noreply.github.com>
2020-05-30 13:03:42 +0530

    Created Prime Checker

---
+++ New folder/PrimeCheck/src/PrimeCheck.scala
@@ -0,0 +1,29 @@
+import scala.io.StdIn.readInt;
+
+object PrimeCheck {
+  
+  def gcd(a:Int,b:Int):Int=b match{
+     case 0 => a
+     case b if (b>a) => gcd(b,a)
+     case _ => gcd(b,a%b) 
+  }
+  
+     def primeCheck(p:Int,n:Int=2):Boolean=n match{
+     case x if(x==p) => true
+     case x if(gcd(p,x)>1) => false
+     case x => primeCheck(p,x+1)
+   }
+  
+     def main(args:Array[String]){
+       println("Insert the Number that want t check : ");
+       var numb:Int=readInt();
+       
+       if(primeCheck(numb)){
+         println(numb+" is a prime Number")
+       }
+       else {
+         println(numb+" is not a prime Number")
+       }
+       
+     }
+}
