c8ede0c43a59a94b71377deb91db5fe8e1af57cf
uruwanara <uruwanara@users.noreply.github.com>
2020-05-30 14:45:50 +0530

    Edited

--- New folder/PrimeSequence/src/PrimeSequence.scala
+++ New folder/PrimeSequence/src/PrimeSequence.scala
@@ -1,5 +1,39 @@
-
+import scala.io.StdIn.readInt;
 
 object PrimeSequence {
+  def gcd(a:Int,b:Int):Int=b match{
+     case 0 => a
+     case b if (b>a) => gcd(b,a)
+     case _ => gcd(b,a%b) 
+  }
   
+     def primeCheck(p:Int,n:Int=2):Boolean=n match{
+     case x if(x==p) => true
+     case x if(gcd(p,x)>1) => false
+     case x => primeCheck(p,x+1)
+   }
+    
+     def printSeq(numb:Int):Unit={
+     if(numb>1){
+          if(primeCheck(numb)==true){
+             print(numb+"\t");
+             printSeq(numb-1);
+          }
+          else{
+             printSeq(numb-1);
+          }
+     
+     }
+
+   }
+  
+     def main(args:Array[String]){
+       println("Insert the Number upper limt ofsequence : ");
+       var numb:Int=readInt();
+       
+       println("Prime Sequence until "+numb+" :" );
+       printSeq(numb);
+       
+     }
 }
+       
